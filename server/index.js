fcf3f866be5ff8a5afb3a5707b47bf28cecd10a4
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-03-02 12:46:05 +0530

    user model is done

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "/controllers/auth.js"
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -36,6 +37,9 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage });
+
+// Routes With File
+app.post("auth/register",upload.single("picture"),register);
 
 // mongoose set up
 const port = process.env.PORT || 6001
