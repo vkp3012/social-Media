48a95fa68944d94ed6f006fccead78b4e62ebf54
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-03-10 14:40:24 +0530

    all file modified

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,72 +1,62 @@
-import Post from "../models/Post.js";
 import User from "../models/User.js";
 
-/* CREATE */
-export const createPost = async (req, res) => {
-  try {
-    const { userId, description, picturePath } = req.body;
-    const user = await User.findById(userId);
-    const newPost = new Post({
-      userId,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      location: user.location,
-      description,
-      userPicturePath: user.picturePath,
-      picturePath,
-      likes: {},
-      comments: [],
-    });
-    await newPost.save();
-
-    const post = await Post.find();
-    res.status(201).json(post);
-  } catch (err) {
-    res.status(409).json({ message: err.message });
-  }
-};
-
 /* READ */
-export const getFeedPosts = async (req, res) => {
+export const getUser = async (req, res) => {
   try {
-    const post = await Post.find();
-    res.status(200).json(post);
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
 
-export const getUserPosts = async (req, res) => {
+export const getUserFriends = async (req, res) => {
   try {
-    const { userId } = req.params;
-    const post = await Post.find({ userId });
-    res.status(200).json(post);
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
 
 /* UPDATE */
-export const likePost = async (req, res) => {
+export const addRemoveFriend = async (req, res) => {
   try {
-    const { id } = req.params;
-    const { userId } = req.body;
-    const post = await Post.findById(id);
-    const isLiked = post.likes.get(userId);
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
 
-    if (isLiked) {
-      post.likes.delete(userId);
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
     } else {
-      post.likes.set(userId, true);
+      user.friends.push(friendId);
+      friend.friends.push(id);
     }
+    await user.save();
+    await friend.save();
 
-    const updatedPost = await Post.findByIdAndUpdate(
-      id,
-      { likes: post.likes },
-      { new: true }
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
     );
 
-    res.status(200).json(updatedPost);
+    res.status(200).json(formattedFriends);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
